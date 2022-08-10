f2e8ada8b5ce8794575d9693dd8ed434fb9c26a3
usman-yello <106993381+usman-yello@users.noreply.github.com>
2022-08-10 09:13:29 -0400

    update compat.py

--- suit/compat.py
+++ suit/compat.py
@@ -1,6 +1,23 @@
+import django
+
 try:
-    # Python 3.
-    from urllib.parse import parse_qs
+    from importlib import import_module
+except ImportError:  # python = 2.6
+    from django.utils.importlib import import_module  # Django < 1.9
+
+if django.VERSION[:2] < (1, 5):
+    from django.templatetags.future import url
+else:
+    from django.template.defaulttags import url
+
+try:
+    from django.contrib.contenttypes import admin as ct_admin
 except ImportError:
-    # Python 2.6+
-    from urlparse import parse_qs
+    from django.contrib.contenttypes import generic as ct_admin  # Django < 1.8
+
+if django.VERSION[:2] < (1, 8):
+    from django.template import Context
+
+    tpl_context_class = Context
+else:
+    tpl_context_class = dict
